a942b1af7c197ed1d8b69dd18d69e20fbb03382b
rsthind14-cloud <rsthind14@gmail.com>
2025-12-25 20:13:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import { BookOpen, Clock, Zap, CheckCircle, Menu, X, Star, Camera, Image as Imag
 // --- CONFIGURATION ---
 // IMPORTANT: Jab tu isse apni website par daale, toh apni asli API Key yahan quotes mein daal dena.
 // Example: const apiKey = "AIzaSy...";
-const apiKey = 
-  import.meta.env.VITE_GEMINI_API_KEY
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 
 const App = () => {
   const [messages, setMessages] = useState([
